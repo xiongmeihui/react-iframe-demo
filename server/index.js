e1363aca63215baf404d1905ead7a3bbf25b5c87
xiongmeihui <xiongmeihui@youzan.com>
2020-06-25 21:46:55 +0800

    fix: 搭好框架

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer();
+
+server.listen(9000, function () {
+  console.log("http server is listening at port 9000");
+});
+
+server.on("request", function () {
+  console.log("收到客户发出的请求");
+});
+
+server.on("request", function (req, res) {
+  const { url } = req;
+  if (url === "/") {
+    //response.writeHead(响应状态码，响应头对象): 发送一个响应头给请求。
+    res.writeHead(200, { "Content-Type": "text/html" });
+    // 如果url=‘/’ ,读取指定文件下的html文件，渲染到页面。
+    fs.readFile(path.resolve(__dirname, "./demo.html"), "utf-8", function (
+      err,
+      data
+    ) {
+      if (err) {
+        throw err;
+      }
+      res.end(data);
+    });
+  }
+});
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div class="left">导航栏</div>
+      <div class="content">
+        {/* <iframe id="child" src="http://172.18.213.40:9000"></iframe> */}
+      </div>
+      <div class="right">帮助栏</div>
+    </div>
+  );
+}
+
+export default App;
