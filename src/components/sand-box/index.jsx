096130567009037360f46e6e4f46dfa5c4e6a5e5
xiongmeihui <xmh9368@gmail.com>
2020-06-27 22:19:28 +0800

    feat: 父子传值

--- src/components/sand-box/index.jsx
+++ src/components/sand-box/index.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useRef } from "react";
+import "./style.css";
 
 export default function ({ src, id }) {
   const iframeRef = useRef(null);
   useEffect(() => {
     const innerWindow = iframeRef.current.contentWindow;
-    innerWindow.postMessage("测试", src);
+    console.log(document.readyState);
+    console.log(innerWindow.document.readyState);
+    document.onreadystatechange = function () {
+      if (document.readyState === "complete") {
+        innerWindow.postMessage({ userId: 1, userName: "xmh" }, src);
+      }
+    };
   }, [src]);
-  return <iframe ref={iframeRef} src={src} title={id} />;
+  return (
+    <iframe
+      className="sand-box-wrapper"
+      ref={iframeRef}
+      src={src}
+      title={id}
+    ></iframe>
+  );
 }
