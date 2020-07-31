c8b838c8fd3658e9cd412c2cd6be0ce707e92790
xiongmeihui <xiongmeihui@youzan.com>
2020-07-31 15:23:20 +0800

    feat: 增加接受信息js包

--- server/test.js
+++ server/test.js
@@ -1,4 +1,28 @@
-const container = document.getElementById("box");
-console.log(container);
-container.innerHTML = "收到信息";
-console.log(window.userInfo);
+parent.postMessage({ pageType: "THIRD_PARTY_APP" }, "*");
+const loadScript = function (src) {
+  const script = document.createElement("script");
+  script.src = src;
+  return script;
+};
+
+const loadJsResource = function () {
+  const urlArr = window.resourceUrl ? Object.values(window.resourceUrl) : [];
+  const fragment = document.createDocumentFragment();
+  urlArr.forEach((url) => {
+    fragment.appendChild(loadScript(url));
+  });
+  return fragment;
+};
+
+const handleMessage = function (event) {
+  if (/.*\.youzan\.com$/.test(event.orgin)) {
+    const { userToken } = event.data;
+    window.userToken = userToken;
+
+    document.head.appendChild(loadJsResource());
+
+    window.removeEventListener("message", handleMessage, false);
+  }
+};
+// 收到来自宿主页面的消息
+window.addEventListener("message", handleMessage, false);
